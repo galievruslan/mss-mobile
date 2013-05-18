bb53715ab5799471aa0b0e6f56fbdf1b0cf65fd9
golfarid <golfarid@gmail.com>
2013-05-18 20:27:25 +0400

    Cgange ShippingAddressListBoxItem, add address to output Close #4

--- MSS.WinMobile/MSS.WinMobile.UI.Controls.Concret/ListBoxItems/ShippingAddressListBoxItem.cs
+++ MSS.WinMobile/MSS.WinMobile.UI.Controls.Concret/ListBoxItems/ShippingAddressListBoxItem.cs
@@ -10,19 +10,21 @@ namespace MSS.WinMobile.UI.Controls.Concret.ListBoxItems
             InitializeComponent();
         }
 
+        private System.Windows.Forms.LinkLabel _addressLabel;
+
         private ShippingAddressViewModel _viewModel;
         public ShippingAddressViewModel ViewModel
         {
             get { return _viewModel; }
             set {
                 _viewModel = value;
-                _nameLabel.Text = ViewModel.Address;
+                _nameLabel.Text = ViewModel.Name;
+                _addressLabel.Text = ViewModel.Address;
             }
         }
 
-        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
-        {
-            _nameLabel.BackColor = IsSelected ? ColorSelected : ColorUnselected;
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e) {
+            _nameLabel.BackColor = _addressLabel.BackColor = IsSelected ? ColorSelected : ColorUnselected;
             base.OnPaint(e);
             DrawDivisor(e.Graphics);
         }
@@ -30,6 +32,7 @@ namespace MSS.WinMobile.UI.Controls.Concret.ListBoxItems
         private void InitializeComponent()
         {
             this._nameLabel = new System.Windows.Forms.LinkLabel();
+            this._addressLabel = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // _nameLabel
@@ -39,16 +42,27 @@ namespace MSS.WinMobile.UI.Controls.Concret.ListBoxItems
             this._nameLabel.ForeColor = System.Drawing.Color.Black;
             this._nameLabel.Location = new System.Drawing.Point(0, 0);
             this._nameLabel.Name = "_nameLabel";
-            this._nameLabel.Size = new System.Drawing.Size(200, 28);
+            this._nameLabel.Size = new System.Drawing.Size(200, 14);
             this._nameLabel.TabIndex = 0;
-            this._nameLabel.TabStop = false;
             this._nameLabel.Text = "linkLabel1";
             this._nameLabel.Click += new System.EventHandler(this.LabelClick);
+            // 
+            // _addressLabel
+            // 
+            this._addressLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this._addressLabel.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular);
+            this._addressLabel.ForeColor = System.Drawing.Color.Black;
+            this._addressLabel.Location = new System.Drawing.Point(0, 14);
+            this._addressLabel.Name = "_addressLabel";
+            this._addressLabel.Size = new System.Drawing.Size(200, 14);
+            this._addressLabel.TabIndex = 1;
+            this._addressLabel.Text = "linkLabel1";
             // 
             // ShippingAddressListBoxItem
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this._addressLabel);
             this.Controls.Add(this._nameLabel);
             this.Name = "ShippingAddressListBoxItem";
             this.Size = new System.Drawing.Size(200, 30);
